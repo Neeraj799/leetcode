1841ce8c9205082e42e7160c194f28b0238b5a43
neeraj <neerajpc799@gmail.com>
2025-11-06 19:33:05 +0530

    Added dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,22 @@ import { useUser } from "@clerk/clerk-react";
 import Homepage from "./pages/Homepage";
 import ProblemsPage from "./pages/ProblemsPage";
 import { ToastContainer } from "react-toastify";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
   return (
     <>
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <Homepage /> : <Navigate to={"/dashboard"} />}
+        />
+
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route
           path="/problems"
           element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
