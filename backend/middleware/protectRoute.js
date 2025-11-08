8d3b114ee4c2b34d083578aad030bb1155a7f018
neeraj <neerajpc799@gmail.com>
2025-11-08 22:05:49 +0530

    Chnages in server.js

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,12 +1,11 @@
 import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
-import envConfig from "../config/envConfig.js";
 
 export const protectRoute = [
   requireAuth(),
   async (req, res, next) => {
     try {
-      const clerkId = req.auth().userId;
+      const clerkId = req.auth?.userId;
 
       if (!clerkId)
         return res.status(401).json({ msg: "Unauthorized - invalid token" });
